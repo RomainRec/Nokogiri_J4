81226a005954713fdf1a59ad5839af58eb59001e
RomainRec <43348150+RomainRec@users.noreply.github.com>
2018-10-11 20:18:43 +0200

    Update README.rb

--- README.rb
+++ README.rb
@@ -1,4 +1,4 @@
-Salut correcteur ! Voici les 3 exercices THP jour 4 - semaine 02 :
+Salut correcteur(ice) ! Voici les 3 exercices THP jour 4 - semaine 02 :
 https://github.com/RomainRec/Nokogiri_J4/new/master
 
 Fait en pair-prog avec Hai Dang.
